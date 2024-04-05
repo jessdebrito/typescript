ebb710313a80f2285b4b76a072109618b7fe2f92
jessdebrito <debrito.herself@gmail.com>
2024-04-04 23:28:17 -0300

    primitives typescript

--- 1 - Introdução do Módulo + Typescript (Parte 1)/primitives.ts
+++ 1 - Introdução do Módulo + Typescript (Parte 1)/primitives.ts
@@ -3,9 +3,9 @@
 
 // Podemos apontar um tipo, devemos apontar um tipo:
 // PODEMOS apontar tipos em instâncias de variável e constantes
- // let firstName:string = 123; // erro!!! 
-let firstName2:string = "Jéssica"; // certo!
-let firstName3:boolean = true;  // certo!
+ // let firstName: string = 123; // erro!!! 
+let firstName2: string = "Jéssica"; // certo!
+let firstName3: boolean = true;  // certo!
 
 
 let firstName4 = 123; // certo!!! 
@@ -15,3 +15,12 @@ let firstName6 = true;  // certo!
 // typescript não quer deixar o codigo muito verboso,
 // por isso há uma inteligência.
 // Inferência - tenta deduzir, de forma expontânea o "tipo".
+
+// DEVEMOS  apontar tipos para os parâmetros das nossas funções. exemplo:
+ function sub(a: number, b: number){
+    return a - b;
+ }
+
+ function sayMyName(name: string){
+    console.log(name);
+ }
