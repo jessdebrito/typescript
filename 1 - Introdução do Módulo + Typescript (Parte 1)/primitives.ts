7b3f7dad1dfbb89c1ff8606dbe89b204120f36a4
jessdebrito <debrito.herself@gmail.com>
2024-04-04 23:23:37 -0300

    primitives typescript

---
+++ 1 - Introdução do Módulo + Typescript (Parte 1)/primitives.ts
@@ -0,0 +1,17 @@
+// Tipos primitivos
+// valores primitivos: string, number, boolean, undefined, null
+
+// Podemos apontar um tipo, devemos apontar um tipo:
+// PODEMOS apontar tipos em instâncias de variável e constantes
+ // let firstName:string = 123; // erro!!! 
+let firstName2:string = "Jéssica"; // certo!
+let firstName3:boolean = true;  // certo!
+
+
+let firstName4 = 123; // certo!!! 
+let firstName5 = "Jéssica"; // certo!
+let firstName6 = true;  // certo!
+
+// typescript não quer deixar o codigo muito verboso,
+// por isso há uma inteligência.
+// Inferência - tenta deduzir, de forma expontânea o "tipo".
