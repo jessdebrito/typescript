5ac7d51045f32e73963363f0f2891ddfbc1b7252
jessdebrito <debrito.herself@gmail.com>
2024-04-09 14:48:48 -0300

    this

--- 2 - Sprint/app/src/index.ts
+++ 2 - Sprint/app/src/index.ts
@@ -6,8 +6,47 @@
 // Toda classe também inicie com uma letra maiúscula
 // PascalCase
 
+/* --------------
 
-class Person{
-    name = "Jéssica";
-    age = 29;
+let firstName = "Jéssica";
+function sayHello (name: string){
+    console.log(firstName);
+} 
+
+-------------- */ 
+
+// this - é utilizado para se referir a própria classe e consegue acessar varicáveis e métodos
+
+// Planta de um "objeto"
+// a classe deve ser escalável
+
+/* --------------
+
+class Person {
+    name = "Jéssica"; // qualquer nome
+    age = 29; // qualquer idade
+
+    sayHello() {
+        console.log(`Olá, meu nome é ${this.name}!`);
+    }
+}
+
+-------------- */ 
+
+// Método construtor:
+
+class Person {
+    name: string; // qualquer nome
+    age: number; // qualquer idade
+
+    //método que executa na inicialização de uma classe
+    constructor(name: string, age: number){
+        this.name = name;
+        this.age = age;
+
+    }
+
+    sayHello() {
+        console.log(`Olá, meu nome é ${this.name}!`);
+    }
 }
