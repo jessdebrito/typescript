c5c6c05600fc2458026433c94c39b3a9e0834721
jessdebrito <debrito.herself@gmail.com>
2024-04-09 14:14:15 -0300

    POO

---
+++ 2 - Sprint/app/src/index.ts
@@ -0,0 +1,13 @@
+// POO - Programação orientada a Objetos e tipos derivados
+// Classes - encapsula variáveis e métodos (funções)
+// Organização de código
+// JavaScript / Typescript
+
+// Toda classe também inicie com uma letra maiúscula
+// PascalCase
+
+
+class Person{
+    name = "Jéssica";
+    age = 29;
+}
